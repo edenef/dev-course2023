bb95eec1343945ba423edca92a7e2d25e9ed12ee
edenef <eden.efrati.1@gmail.com>
2023-04-28 12:05:32 +0000

    add data

--- helloworld/application.py
+++ helloworld/application.py
@@ -2,6 +2,7 @@
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+import requests
 
 application = Flask(__name__)
 
@@ -12,6 +13,21 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+@application.route('/check/<string:currency>', methods=['GET'])
+def get_currency_check(currency):
+    return Response(json.dumps({'result': currency_rate.get(currency)}), mimetype='application/json', status=200)
+
+@application.route('/check/bitcoin', methods=['GET'])
+def get_bitcoin():
+    res = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+    return Response(json.dumps(res.taxt), mimetype='application/json', status=200)
+
+currency_rate = {
+    'usd' : 3.3,
+    'pound' : 4.5,
+    'euro' : 4.8
+}
 
 if __name__ == '__main__':
     flaskrun(application)
